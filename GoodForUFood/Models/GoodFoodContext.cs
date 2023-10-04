c2f148a4e9f34c3f6586e9425d65db49d0271931
Darius Robinson <robinson.darius090@gmail.com>
2023-10-03 23:18:46 -0400

    working on getting ef core set up using mysql

---
+++ GoodForUFood/Models/GoodFoodContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodForUFood.Models
+{
+	public class GoodFoodContext : DbContext 
+	{
+		public GoodFoodContext(DbContextOptions<GoodFoodContext>options) : base(options)
+		{
+		}
+
+		public DbSet<FoodItem> FoodItems { get; set; }
+
+		public DbSet<Category> Categories { get; set; }
+	}
+}
+
